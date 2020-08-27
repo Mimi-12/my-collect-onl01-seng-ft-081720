3d8460d022b2f3da75584c142909bdf616b3cc7e
selam lemma <oneiconforlife26@gmail.com>
2020-08-27 15:55:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,18 @@
-
-def collect( collection)
-
+def my_collect(collection)
+  i = 0
+#  collection = []
+  while i < collection .length
+    #collection <<
+     yield(collection[i])
+    i += 1
+  end
+  collection
 end
 
 
-def my_collect
 
-end
+
+
+
+
+
